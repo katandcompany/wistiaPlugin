1f254065607b0f25ef367c43b19fd7797192baf0
KAT & Company <webmaster@katandcompany.com>
2021-10-04 22:02:34 -0500

    Added styles related to bookmark icons.

--- src/widget/index.js
+++ src/widget/index.js
@@ -1,13 +1,13 @@
 import './widget.less';
 import React from 'react';
 import { render } from 'react-dom';
-import { WistiaProvider } from '../contexts/WistiaContext';
+import { PluginProvider } from '../contexts/PluginContext';
 import Widget from './components/Widget';
 
 const target = document.getElementById('mount');
 render(
-  <WistiaProvider>
+  <PluginProvider>
     <Widget />
-  </WistiaProvider>,
+  </PluginProvider>,
   target
 );
